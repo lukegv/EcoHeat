ff5382a531f93dfebbbecb10edd3d79e5ad1e373
Lukas Körfer <lu.koerfer@googlemail.com>
2016-04-29 21:04:10 +0200

    Added SettingsActivity

--- Android/HackathonViessmann/app/src/main/java/de/lukaskoerfer/hackathonviessmann/MainActivity.java
+++ Android/HackathonViessmann/app/src/main/java/de/lukaskoerfer/hackathonviessmann/MainActivity.java
@@ -1,9 +1,11 @@
 package de.lukaskoerfer.hackathonviessmann;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import de.lukaskoerfer.hackathonviessmann.weather.OpenWeather;
 
@@ -21,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = this.getMenuInflater();
         inflater.inflate(R.menu.menu_options, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_settings:
+                this.startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return false;
+        }
     }
 }
