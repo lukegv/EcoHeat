90c8609a24b70cf82751da52c5becc7b5ce4f80d
Eugen Salzmann <Eugen Salzmann>
2016-04-30 06:15:32 +0200

    not done yet

--- Android/HackathonViessmann/app/src/main/java/de/lukaskoerfer/hackathonviessmann/model/HeatingPattern.java
+++ Android/HackathonViessmann/app/src/main/java/de/lukaskoerfer/hackathonviessmann/model/HeatingPattern.java
@@ -1,5 +1,7 @@
 package de.lukaskoerfer.hackathonviessmann.model;
 
+import android.content.Context;
+
 import java.util.List;
 
 /**
@@ -7,18 +9,26 @@ import java.util.List;
  */
 public class HeatingPattern {
 
-    public void setNormalMode(List<PredictionDataPoint> predictionData){
+    public void setEcoMode(List<PredictionDataPoint> predictionData, Context context){
+        UserPreferences prefs = UserPreferences.FromSettings(context);
         int n = predictionData.size();
         float time = 0;
         float temp;
         for (int i=0;i<n;i++) {
             temp = predictionData.get(i).getTime()+time %(60*60*24);
-            if ( temp <=  && temp >= ) {
-                predictionData.get(i).setTargetTemperature();
+            if ( temp <= prefs.getWeekDayStart() && temp >= prefs.getWeekDayEnd()) {
+                predictionData.get(i).setTargetTemperature(prefs.getNightTemperature());
             }
             else{
-                predictionData.get(i).setTargetTemperature();
+                predictionData.get(i).setTargetTemperature(prefs.getDayTemperature());
             }
         }
+    }
+
+    public void setNormalMode(List<PredictionDataPoint> predictionData, Context context){
+        UserPreferences prefs = UserPreferences.FromSettings(context);
+        int n = predictionData.size();
+        for (int i=0;i<n;i++) { predictionData.get(i).setTargetTemperature(prefs.getDayTemperature());}
+
     }
 }
