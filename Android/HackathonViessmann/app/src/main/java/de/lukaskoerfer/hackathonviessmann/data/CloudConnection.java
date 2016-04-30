489986921e940cd0f2fce018a57d1f90c887446c
Lukas Körfer <lu.koerfer@googlemail.com>
2016-04-30 10:07:37 +0200

    Implemented HistoryActivity

--- Android/HackathonViessmann/app/src/main/java/de/lukaskoerfer/hackathonviessmann/data/CloudConnection.java
+++ Android/HackathonViessmann/app/src/main/java/de/lukaskoerfer/hackathonviessmann/data/CloudConnection.java
@@ -9,24 +9,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import de.lukaskoerfer.hackathonviessmann.model.HistoryElement;
 import de.lukaskoerfer.hackathonviessmann.model.UserData;
-import de.lukaskoerfer.hackathonviessmann.model.WeatherForecast;
 
 /**
  * Created by Koerfer on 30.04.2016.
  */
 public class CloudConnection {
 
-    private static final String CLOUD_URL = "http://hackathon-viessmann.azurewebsites.net/userdata.php?";
+    private static final String USERDATA_URL = "http://hackathon-viessmann.azurewebsites.net/userdata.php?";
 
     public static UserData GetUserData(String username) {
         try {
             String usernameValue = "user=" + username;
-            String urlString = CLOUD_URL + usernameValue;
+            String urlString = USERDATA_URL + usernameValue;
             URL requestUrl = new URL(urlString);
             InputStream stream = requestUrl.openStream();
             Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream);
@@ -41,6 +43,32 @@ public class CloudConnection {
             saxex.printStackTrace();
         }
         return null;
+    }
+
+    private static final String HISTORY_URL = "http://hackathon-viessmann.azurewebsites.net/housedata.php?";
+
+    public static List<HistoryElement> GetHistory(int houseId) {
+        List<HistoryElement> history = new ArrayList<>();
+        try {
+            String usernameValue = "houseid=" + Integer.toString(houseId);
+            String urlString = HISTORY_URL + usernameValue;
+            URL requestUrl = new URL(urlString);
+            InputStream stream = requestUrl.openStream();
+            Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream);
+            NodeList nodes = doc.getElementsByTagName("entry");
+            for (int i = 0; i < nodes.getLength(); i++) {
+                history.add(new HistoryElement((Element) nodes.item(i)));
+            }
+        } catch (MalformedURLException muex) {
+            muex.printStackTrace();
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+        } catch (ParserConfigurationException pcex) {
+            pcex.printStackTrace();
+        } catch (SAXException saxex) {
+            saxex.printStackTrace();
+        }
+        return history;
     }
 
 }
